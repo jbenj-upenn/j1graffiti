a115021276eb90129b611c1d470ea8aec5323da1
Joshua Benjamin <jbenj@sas.upenn.edu>
2021-04-08 13:33:24 -0400

    add bootstrap and react packages; begin connecting files to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router } from "react-router-dom";
+import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
+      <Router >
+        <Header />
+
+        
+      </Router>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
