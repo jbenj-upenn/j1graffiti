c8dd2cf6347f708aee8ff5be9e2bceddb11dbfa4
Joshua Benjamin <jbenj@sas.upenn.edu>
2021-04-08 13:42:44 -0400

    changing quick links in NavBar

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,20 +6,20 @@ function Header(props) {
     return (
 
         <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="/">Joshua Benjamin Professional Portfolio</Navbar.Brand>
+            <Navbar.Brand href="/">Joshua Benjamin's Graffiti Portfolio</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
                     {/* <Nav.Link href="#portfolio">Portolio</Nav.Link> */}
 
                     <NavDropdown title="Portfolio Quick Links" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="https://goal-getters.herokuapp.com/">Goal-Getter App</NavDropdown.Item>
-                        <NavDropdown.Item href="https://breaking-bread-donate.herokuapp.com/">Breaking Bread App</NavDropdown.Item>
-                        <NavDropdown.Item href="https://jbenj-upenn.github.io/CovidActivities/">Covid Activities App</NavDropdown.Item>
-                        <NavDropdown.Item href="https://jbenj-upenn.github.io/personal-profile-v2020/">Profile with HTML/CSS</NavDropdown.Item>
-                        <NavDropdown.Item href="https://immense-river-52204.herokuapp.com/">Burger Smash</NavDropdown.Item>
-                        <NavDropdown.Item href="https://expressnotetaker-jbenj.herokuapp.com/">Express Note-Taker</NavDropdown.Item>
-                        <NavDropdown.Item href="https://jbenj-upenn.github.io/WeatherOutlook_6/">Weather Outlook</NavDropdown.Item>
+                        <NavDropdown.Item href="#">2559 Amber St., Philadelphia</NavDropdown.Item>
+                        <NavDropdown.Item href="#">Relentless, Doylestown</NavDropdown.Item>
+                        <NavDropdown.Item href="#">Bike Works, Lititz</NavDropdown.Item>
+                        <NavDropdown.Item href="#">Moses, San Diego</NavDropdown.Item>
+                        <NavDropdown.Item href="#">Doulin's Garage, Holtwood</NavDropdown.Item>
+                        <NavDropdown.Item href="#">Lake Street, Lancaster</NavDropdown.Item>
+                        <NavDropdown.Item href="#">The Hive, Lancaster</NavDropdown.Item>
                     </NavDropdown>
                 </Nav>
             </Navbar.Collapse>
